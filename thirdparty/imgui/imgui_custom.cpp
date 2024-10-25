6e9f664b5e158be70e19411d540ef40637fa1528
Ansari <ansari_official@yahoo.com>
2024-10-26 00:29:01 +0530

    Refactor RGBAtoIV4 function in imgui_custom.cpp for improved readability and precision

--- thirdparty/imgui/imgui_custom.cpp
+++ thirdparty/imgui/imgui_custom.cpp
@@ -1,10 +1,10 @@
 #include "imgui_custom.h"
-
+#include <iostream>
 ImVec4 RGBAtoIV4(int r, int g, int b, float a) noexcept
 {
-	float newr = r / 255;
-	float newg = g / 255;
-	float newb = b / 255;
+	float newr = static_cast<float>(r) / 255.0f;
+	float newg = static_cast<float>(g) / 255.0f;
+	float newb = static_cast<float>(b) / 255.0f;
 	float newa = a;
 	return ImVec4(newr, newg, newb, newa);
 }
