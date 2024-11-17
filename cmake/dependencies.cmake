c0d9776f0af03e32c29e51f6a5d3a91b15326471
Ansari <ansari_official@yahoo.com>
2024-11-17 16:51:43 +0530

    Including OpenSSL libs directly to the project for windows case

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,17 +1,16 @@
 function(configure_openssl)
-    if(WIN32 OR APPLE)
-        if(NOT OPENSSL_ROOT_DIR)
-            log_error("OpenSSL root dir not found. Please set OPENSSL_ROOT_DIR.")
-        else()
-            log_status("OpenSSL root dir: ${OPENSSL_ROOT_DIR}")
-        endif()
+    if(WIN32)
+        log_status("Configuring Pre-built OpenSSL library for Windows.")
+        log_status("If you want to use a different version, set the OPENSSL_ROOT_DIR variable.")
+        set(OPENSSL_ROOT_DIR "thirdparty/openssl")
     endif()
 
-    find_package(OpenSSL REQUIRED)
+    find_package(OpenSSL REQUIRED QUIET)
+
     if(OpenSSL_FOUND)
         log_status("OpenSSL version: ${OPENSSL_VERSION}")
     else()
-        log_error("OpenSSL not found!")
+        log_error("OpenSSL not found! Please install it or set the OPENSSL_ROOT_DIR variable.")
     endif()
 endfunction()
 
@@ -20,13 +19,13 @@ function(configure_poco)
     set(BUILD_SHARED_LIBS NO CACHE BOOL "" FORCE)
     set(POCO_MT ON CACHE BOOL "" FORCE)
     add_definitions(-DPOCO_NO_AUTOMATIC_LIBS)
-    
+
     function(disable_poco_modules)
         foreach(module ${ARGV})
             set(ENABLE_${module} OFF CACHE BOOL "" FORCE)
         endforeach()
     endfunction()
-    
+
     log_status("Disabling unwanted Poco modules")
     disable_poco_modules(
         JWT DATA DATA_MYSQL DATA_POSTGRESQL DATA_ODBC
@@ -37,9 +36,9 @@ function(configure_poco)
     )
 
     set(REQUIRED_MODULES "UTIL;NET;NETSSL;CRYPTO")
+
     foreach(MODULE ${REQUIRED_MODULES})
         set(ENABLE_${MODULE} ON CACHE BOOL "" FORCE)
-        log_status("Enabled Poco module: ${MODULE}")
     endforeach()
 endfunction()
 
